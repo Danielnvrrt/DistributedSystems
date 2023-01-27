a72927dc3d885f9a2b6558fbed6af39dbd8acb86
Daniel <danielnavatin@gmail.com>
2023-01-27 12:07:12 -0700

    Updated ChatServerWorker.java

---
+++ ChatServerWorker.java
@@ -0,0 +1,24 @@
+@Override
+public void run(){
+    NodeInfo participantInfo = null;
+    Iterator <NodeInfo> participantsIterator;
+
+    try{
+        // get object streams
+        writeToNet = new ObjectOUtputStream(chatConnection.getOutputStream());
+        readFromNet = new ObjectInputStream(chatConnection.getInputStream());
+
+        // read message
+        message = (Message) readFromNet.readObject();
+
+        chatConnection.close();
+    }
+    catch (IOException | ClassNotFoundException e)
+    {
+        System.out.println("[ChatServerWorker.run] Failed to open object stream");
+
+        System.exit(1);
+    }
+
+    //processing message
+}
