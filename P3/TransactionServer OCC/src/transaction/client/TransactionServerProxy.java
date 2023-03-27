193487d5db0720d0a2389aff363ecc6051d28033
Brian Ruiz <bsruiz2001@gmail.com>
2023-03-27 10:29:39 -0700

    I have finished the TransactionServerProxy.java, it has all the functions and I added comments to what they will do from what I saw in the video.

--- P3/TransactionServer OCC/src/transaction/client/TransactionServerProxy.java
+++ P3/TransactionServer OCC/src/transaction/client/TransactionServerProxy.java
@@ -8,6 +8,42 @@ package transaction.client;
  *
  * @author Usuario
  */
-public class TransactionServerProxy {
+public class TransactionServerProxy implements MessageTypes {
     
+    /*
+    *Constructor for serve proxy
+    *
+    */
+    TransactionServerProxy(String host, int port){
+        // contructs the server using the given information
+    }
+    
+    //opens a transaction
+    public int openTransaction() {
+        // this is where we will create the transaction
+        // that will start a connection and also record the message
+        
+        // the transaction will return the transaction id
+    }
+    
+    // closes a transaction
+    // it will request this transaction to close
+    public int closeTransaction() {
+        // records the message in the transaction
+        // return that the transaction has been committed
+    }
+    
+    //reads a value from an account
+    public int read(int accountNumber) throws TransactionAbortedException {
+        // create message variable
+        // read the content
+        // return the content of the message or abort
+    }
+    
+    // writes a value to an account
+    public void write(int accountNumber, int amount) throws TransactionAbortedException {
+        // create message and content variables
+        // write the content
+        // throw exception if aborted
+    }
 }
