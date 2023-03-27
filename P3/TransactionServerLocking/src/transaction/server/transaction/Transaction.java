d659afbc7547644e973da0afa3365eb6206ce9b6
Daniel <danielnavatin@gmail.com>
2023-03-26 18:38:49 -0700

    P3 project structure

---
+++ P3/TransactionServerLocking/src/transaction/server/transaction/Transaction.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package transaction.server.transaction;
+
+import transaction.server.lock.*;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Transaction {
+    
+}
