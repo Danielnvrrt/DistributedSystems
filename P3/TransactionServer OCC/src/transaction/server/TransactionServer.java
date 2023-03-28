8693d4e78e83928b42af7a4d563d688e6c79ee20
Daniel <danielnavatin@gmail.com>
2023-03-27 19:34:07 -0700

    Transaction Server file

--- P3/TransactionServer OCC/src/transaction/server/TransactionServer.java
+++ P3/TransactionServer OCC/src/transaction/server/TransactionServer.java
@@ -1,18 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package transaction.server;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
- * @author Usuario
+ * @author Daniel
  */
 public class TransactionServer {
 
-    /**
-     * @param args the command line arguments
-     */
+    String serverIP;
+    int serverPort;
+
+    private ServerSocket server = null;
+
+    
+    // Transaction Server constructor, initialize the server socket
+    public TransactionServer(String serverIP, int serverPort) {
+        
+    }
+
+    // Create the new TransactionManager
+    public void run() throws IOException {
+        while(true)
+        {
+            new TransactionManager(server.accept()).start();
+        }
+    }
+    
+    // Main method that creates a new instance of the Transaction Server
+    // with the values taken from the configuration file 
     public static void main(String[] args) {
         // TODO code application logic here
     }
