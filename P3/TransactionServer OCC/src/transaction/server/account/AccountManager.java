06fc279bb3621170236dc12f8a0d4c6659c13971
0ddAuto00 <112899312+0ddAuto00@users.noreply.github.com>
2023-03-27 21:20:44 -0700

    Update AccountManager.java

--- P3/TransactionServer OCC/src/transaction/server/account/AccountManager.java
+++ P3/TransactionServer OCC/src/transaction/server/account/AccountManager.java
@@ -6,8 +6,78 @@ package transaction.server.account;
 
 /**
  *
- * @author Usuario
+ * @author Fady
  */
-public class AccountManager {
+public class AccountManager implements MessageTypes {
     
+    // default constructor
+    public AccountManager() {}
+    
+    // helper method that returns accounts still in need of read
+    // returns a list of accounts that have yet to be read
+    public ArrayList<Account> getReadAccounts() {
+        // return the array of accounts needing to be read
+    }
+    
+    // helper method that returns accounts still in need of write
+    // returns a list of accounts that have yet to be write
+    public ArrayList<Account> getWriteAccounts() {
+        // return the array of accounts needing to be write
+    }
+    
+    // Method that writes new balance to the account
+    public void setAccountBalance (int accountID, int accountBalance, int balanceOffset) {
+        // Read account given by ID
+        
+        // Check for validation
+            // abort if needed
+        
+        // Add or subract account's balance by offset
+        
+        // Commit
+    }
+    
+    // Method that reads account details
+    public ArrayList<Account> getAccountDetails (int accountID, int accountBalance, int balanceOffset) {
+        // Read account given by ID
+        
+        // Check for validation
+            // abort if needed
+        
+        // Return details of account
+    }
+    
+    // objects of this inner class run transaction, one thread runs one
+    // transaction on behalf of a client
+    public class AccountManager extends Thread {
+        // create network related variables
+        
+        // create account related variables
+        
+        // set flag for jumping out of while loop after this account has changed
+        
+        // constructor to open up the network channels
+        private AccountManager(Socket client) {
+            // set up object streams
+        }
+    }
+    
+    // run the transaction
+    public void run() {
+        // loop for each account needing to be edited
+        // start of while loop
+        
+            // read the message/command
+            
+            // process the message/command
+            // switch cases
+            
+                // Perform high-level operations on the account
+        
+                // Boolean to check if account changes, if write, have been commited
+        
+            // Validate
+        
+        // end while loop
+    }
 }
