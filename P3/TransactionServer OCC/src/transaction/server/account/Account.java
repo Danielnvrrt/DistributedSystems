6564beee85abb0ea47c86604461614b72fcff7fc
0ddAuto00 <112899312+0ddAuto00@users.noreply.github.com>
2023-03-27 21:05:41 -0700

    Update Account.java

--- P3/TransactionServer OCC/src/transaction/server/account/Account.java
+++ P3/TransactionServer OCC/src/transaction/server/account/Account.java
@@ -2,12 +2,50 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package transaction.server.transaction;
+
+import transaction.server.TransactionServer;
+import java.util.ArrayList;
+import java.util.HashMap;
 package transaction.server.account;
 
 /**
  *
- * @author Usuario
+ * @author Fady
  */
 public class Account {
+    int accountID;
+    int accountBalance;
+    int lastCommittedAccountID;
+    
+    ArrayList<Integer> readSet = new ArrayList<>();
+    HashMap<Integer, Integer> writeSet = new HashMap<>();
+    
+    StringBuffer log = new StringBuffer("");
+    
+    // Constructor for Transaction class
+    // Recieve transactionID and lastCommittedTransactionNumber
+    Account(int accountID, int accountBalance, int lastCommittedAccountID) {
+        this.accountID = accountID;
+        this.accountBalance = accountBalance;
+        this.lastCommittedAccountID = lastCommittedAccountID;
+    }
+    
+    public int readAcc(accountID) {
+        
+    }
+    
+    public int writeAcc(accountID, accountBalance) {
+        
+    }
+    
+    public int getAccountID(){
+        
+    }
+    
+    public int getAccountBalance(accountID) {
+        
+    }
+    
     
 }
