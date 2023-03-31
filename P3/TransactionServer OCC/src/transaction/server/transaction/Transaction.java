02a7d6704564e4675e339440f2c3e39cff4be526
Daniel <danielnavatin@gmail.com>
2023-03-31 10:07:35 -0700

    Added code for Transaction file

--- P3/TransactionServer OCC/src/transaction/server/transaction/Transaction.java
+++ P3/TransactionServer OCC/src/transaction/server/transaction/Transaction.java
@@ -30,31 +30,52 @@ public class Transaction {
     
     // Get the balance of the account with accountNumber
     public int read (int accountNumber){
+        Integer balance;
         
-    }
+        // check if value to be read was written by this transaction
+        balance = writeSet.get(accountNumber);
+        
+        // if not, read the committed version of it
+        if(balance == null){
+            balace = TransactionServer.accountManager.read(accountNumber);
+        }
+        
+        if(!readSet.contains(accountNumber)){
+            readSet.add(accountNumber);
+        }
+        
+        return balance;
+    }   
+    
     
     // Overwrite the newBalance in the account with accountNumber
     public int write (int accountNumber, int newBalance) {
+        int oldBalance = read(accountNumber);
         
+        if (!writeSet.containsKey(accountNumber)){
+            writeSet.put(accountNumber, newBalance);
+        }
+        
+        return oldBalance;
     }
     
     // Get the readSet array
     public ArrayList getReadSet(){
-        
+        return readSet;
     }
     
     // Get the writeSet HashMap
     public HashMap getWriteSet(){
-        
+        return writeSet;
     }
     
     // Get the transactionID
     public int getTransactionID(){
-        
+        return transactionID;
     }
     
     // Get the transactionNumber
     public int getTransactionNumber(){
-        
+        return transactionNumber;
     }
 }
